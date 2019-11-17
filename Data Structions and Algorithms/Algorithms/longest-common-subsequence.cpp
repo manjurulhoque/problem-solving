62c5bd4db342a4c5d6ea3d9d1d7995a758cc1f0d
To Doyel <todoyel19@gmail.com>
2019-11-17 17:16:03 +0600

    0-1 knapsack and LCS

---
+++ Data Structions and Algorithms/Algorithms/longest-common-subsequence.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+int max(int a, int b)
+{
+    return a > b ? a : b;
+}
+
+int lcs(char *x, char *y, int m, int n)
+{
+    int L[m + 1][n + 1];
+    int i, j;
+
+    for(i = 0; i <= m; i++) {
+        for(j = 0; j <= n; j++) {
+            if(i == 0 || j == 0) L[i][j] = 0;
+            else if(x[i - 1] == y[j - 1]) L[i][j] = L[i - 1][j - 1] + 1;
+            else L[i][j] = max(L[i - 1][j], L[i][j - 1]);
+        }
+    }
+
+    return L[m][n];
+}
+
+int main()
+{
+    char X[] = "AGGTAB";
+    char Y[] = "GXTXAYB";
+
+    int m = strlen(X);
+    int n = strlen(Y);
+
+    printf("Length of LCS is %d\n", lcs(X, Y, m, n));
+
+    return 0;
+}
