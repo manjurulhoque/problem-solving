ad897159ee816b060b7f3d547f8e327d8e3a75c8
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2019-12-19 00:33:41 +0600

    Trying 82A

---
+++ Codeforces/82A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<string> ss = {"Sheldon", "Leonard", "Penny", "Rajesh", "Howard"};
+
+
+
+    return 0;
+}
+
