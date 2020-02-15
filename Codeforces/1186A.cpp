7314466e2e829e9be53087d11477aeacf9941449
Rumi <todoyel19@gmail.com>
2020-02-15 18:46:12 +0600

    1186A

---
+++ Codeforces/1186A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/// Accepted
+
+int main()
+{
+    int n, m, k;
+    cin>>n>>m>>k;
+
+    cout<<((n <= m) && (n <= k) ? "Yes" : "No");
+    cout<<'\n';
+
+    return 0;
+}
+
