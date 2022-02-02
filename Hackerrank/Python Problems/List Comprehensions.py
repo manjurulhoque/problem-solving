4803ea2479e3dba3e85ba58cc233471b223ab777
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2022-02-03 00:21:10 +0600

    Hackerrank python list comprehensions using nested list comprehensions

--- Hackerrank/Python Problems/List Comprehensions.py
+++ Hackerrank/Python Problems/List Comprehensions.py
@@ -4,12 +4,11 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
     
-    output = []
 
-    for i in range(x+1):
-        for j in range(y+1):
-            for k in range(z+1):
-                if i + j + k != n:
-                    output.append([i, j, k])
+    # for i in range(x+1):
+    #     for j in range(y+1):
+    #         for k in range(z+1):
+    #             if i + j + k != n:
+    #                 output.append([i, j, k])
 
-    print(output)
+    print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if (i + j + k) != n])
